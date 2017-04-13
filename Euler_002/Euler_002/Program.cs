92c13688eb63d4e4bd832aecddad35e0b347ebbe
stroke-one <irunrap@gmail.com>
2017-04-13 12:01:35 -0700

    Initial commit
    Adding 1 and 2 to solutions

---
+++ Euler_002/Euler_002/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Euler_002
+{
+    class Euler_002
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            int result = 0;
+
+            int maxValue = 4000000;
+            while (c < maxValue)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                if (c % 2 == 0)
+                {
+                    result += c;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
