2ad769b5a419490ed97a17f992270db843c54bc4
stroke-one <irunrap@gmail.com>
2017-04-15 10:37:02 -0700

    Adding 7, updated prime generator

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -8,13 +8,19 @@ namespace ProjectEuler
         static void Main(string[] args)
         {
             Euler001 euler001 = new Euler001(1000);
-            Console.WriteLine(euler001.Solve());
+            Console.WriteLine(string.Format("Euler001\t{0}", (euler001.Solve())));
 
             Euler002 euler002 = new Euler002(4000000);
-            Console.WriteLine(euler002.Solve());
+            Console.WriteLine(string.Format("Euler002\t{0}", (euler002.Solve())));
 
             Euler003 euler003 = new Euler003(600851475143);
-            Console.WriteLine(euler003.Solve());
+            Console.WriteLine(string.Format("Euler003\t{0}", (euler003.Solve())));
+
+            Euler007 euler007 = new Euler007(10001);
+            Console.WriteLine(string.Format("Euler007\t{0}", (euler007.Solve())));
+
+            Euler010 euler010 = new Euler010(2000000);
+            Console.WriteLine(string.Format("Euler010\t{0}", (euler010.Solve())));
         }
     }
 }
